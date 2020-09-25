5cba82b23c9c79e1ffd56563f6643e486f16d599
Arun Kumar <kumar05.962013@gmail.com>
2020-09-25 22:15:12 +0530

    chat box  header and chat section

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,7 +1,43 @@
 import React from "react";
+
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 import "./Chat.css";
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar>A</Avatar>
+        <div className="chat__headerInfo">
+          <h3>Arun </h3>
+          <p>Last Seen at ..</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchIcon fontSize="small" />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon fontSize="small" />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          message
+          <span className="chat__timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+        <p className="chat__message chat__receiver">
+          message
+          <span className="chat__timestamp">
+            {new Date().toLocaleTimeString()}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
